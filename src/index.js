b27316e9ea88a9a2338356cab71ff602f10250d3
Adrij Shikhar <adrijshikhar85@gmail.com>
2020-11-09 23:35:46 +0530

    refactor

--- src/index.js
+++ src/index.js
@@ -10,10 +10,10 @@ import config from './config/config.js';
 const { port } = config;
 const app = express();
 
-app.use(express.json());
-app.use(cors());
-
 app.disable('x-powered-by'); // For security
+
+app.use(cors());
+app.use(express.json());
 app.use(express.urlencoded({ extended: false })); // for proper parsing of query strings
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: false }));
